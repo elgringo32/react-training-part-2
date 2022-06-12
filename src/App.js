bbab98f9d576c3435f4ecd27b6f24bd412666839
Brian Knopke <BrianK@EDIT-2.local>
2022-06-12 12:12:22 -0700

    working props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-const Hello = (props) => {
+const Header = (props) => {
   return (
     <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
+      <h1>{props.course.name}</h1>
+    </div>
+    
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+  return(
+    <div>
+     <p>
+        {props.parts[0].name} {props.parts[0].exercises}
       </p>
+     <p>
+        {props.parts[1].name} {props.parts[1].exercises}
+      </p>
+      <p>
+        {props.parts[2].name} {props.parts[2].exercises}
+      </p>
+  </div>
+  )
+}
+
+const Total = (props) => {
+  return(
+    <div>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
     </div>
   )
 }
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
-  return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </>
-  )
+
+return (
+  <div>
+    <Header course={course} />
+    <Content parts={course.parts}/>
+    <Total parts={course.parts}/>
+
+  </div>
+)
 }
 
 export default App
