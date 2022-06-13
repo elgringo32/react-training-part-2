8773372b4c21c52967450c315e18e0a92d7cf55c
Brian Knopke <BrianK@EDIT-2.local>
2022-06-13 16:40:26 -0700

    added event handling

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 const Header = (props) => {
   return (
     <div>
@@ -31,8 +33,74 @@ const Total = (props) => {
     </div>
   )
 }
+const Display = ({counter}) => <div>{counter}</div> 
+
+const Button = ({onClick, text}) => (
+    <button onClick={onClick}>
+      {text}
+    </button>
+
+)
+
+const Hello = ({name, age}) => {
+  const bornYear = () => new Date().getFullYear() - age
+
+  return (
+    <div>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p>So you were probably born in {bornYear()}</p>
+    </div>
+  )
+}
+
+const History = ({allClicks}) => {
+  if (allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {allClicks.join(' ')}
+    </div>
+  )
+}
+
+const LeftRight = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+  return (
+    <div>
+    {left}
+    <Button onClick={handleLeftClick} text='left' />
+    <Button onClick={handleRightClick} text='right' />
+    {right}
+    <div>
+    <History allClicks={allClicks} />
+    </div>
+  </div>
+
+  )
+}
 
 const App = () => {
+  const name = 'Peter'
+  const age = 10
   const course = {
     name: 'Half Stack application development',
     parts: [
@@ -50,15 +118,37 @@ const App = () => {
       }
     ]
   }
+  const [ counter, setCounter ] = useState(0)
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
 
 
 return (
-  <div>
+  <><div>
     <Header course={course} />
-    <Content parts={course.parts}/>
-    <Total parts={course.parts}/>
-
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
   </div>
+  <div>
+  <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+  </div>
+  <div>
+      <Display counter={counter} />
+      <Button
+        onClick={increaseByOne}
+        text='plus' />
+      <Button
+        onClick={setToZero}
+        text='zero' />
+      <Button
+        onClick={decreaseByOne}
+        text='minus' />
+    </div>
+    <div>
+      <LeftRight />
+      </div></>
 )
 }
 
